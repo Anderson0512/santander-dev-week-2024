8d04b1cb6f4492c5ffac219af4766cd793e2edb1
Anderson0512 <joao_anderson26@hotmail.com>
2024-06-30 19:28:18 -0300

    first commit

---
+++ src/test/java/com/santander/week/SantanderDevWeek2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santander.week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
